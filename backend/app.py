5b33cd87f98496e2c051b6a1c1af1779fc755fcf
Morgwens <carrazanmaria@gmail.com>
2024-07-18 11:39:08 -0300

    agrego cambios en registro backend

--- backend/app.py
+++ backend/app.py
@@ -1,141 +1,41 @@
-import bcrypt 
-import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from app.utils import APIException, generate_sitemap
-from app.models import db
-from app.routes import app
-from app.admin import setup_admin
-from app.commands import setup_commands
-from app.models import Comprador, Vendedor, Moderador, Producto
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from dotenv import load_dotenv
-from flask_mail import Mail, Message
-from app.routes import app
-from datetime import timedelta
-from flask_cors import CORS
-# from models import Pers on
-
-
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] ="sqlite:///project.db"
-
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///proyecto.db"
+app.config["SECRET_KEY"] = "secret_key"
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-jwt = JWTManager(app)
+inicio_sesion = LoginManager()
+inicio_sesion.init_app(app)
+inicio_sesion.login_view = "inicio_sesion"
 
-"""
-class comprador(db.Model):
+class Rol(db.Model):
+    __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    ro
+
+class Usuario(UserMixin, db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    rol = db.Column(db.String(20), nullable=False)
+    rol_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
+    rol = db.relationship("Rol", backref="usuarios")
 
-    def __init__(self, nombre, email, password, rol):
-        self.nombre = nombre
-        self.email = email
-        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
-        self.rol = rol
+    def establecer_pass(self, password):
+        self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password, password)
+    def verificar_pass(self, password):
+        return check_password_hash(self.password, password)
 
-class Moderador(User):
-    __tablename__ = 'moderators'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship("User", backref="moderator")
+@inicio_sesion.user_loader
+def cargar_usuario(usuario_id):
+    return Usuario.query.get(int(usuario_id))
 
-class Vendedor(User):
-    __tablename__ = 'Vendedor'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship("User", backref="seller")
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
-load_dotenv()
-app = Flask(__name__)
-CORS(app)
-app.url_map.strict_slashes = False
-BASE_URL = os.getenv('BACKEND_URL')
-
-#config mail
-app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = '1515c6f1d92b9f'
-app.config['MAIL_PASSWORD'] = '0f8c77ee421a5a'
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app) 
-# Configuración de JWT
-app.config['JWT_SECRET_KEY'] = 'secretisimo'  # Cambia esto por una clave secreta segura
-jwt = JWTManager(app)
-app.url_map.strict_slashes = False
-# database condiguration
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "SQLAlchemy//")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-# app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type=True)
-db.init_app(app)
-
-# add the admin
-setup_admin(app)
-
-# add the admin
-setup_commands(app)
-
-# Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api, url_prefix='/api')
-
-# Handle/serialize errors like a JSON object
-
-
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-
-
-@app.route('/')
-def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
-
-# any other endpoint will try to serve it like a static file
-
-
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
-    return response
-
-
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+from routes import *
--- backend/routes.py
+++ backend/routes.py
@@ -1,67 +1,46 @@
-from flask import Flask, request, jsonify, url_for, Blueprint
-from app.utils import generate_sitemap, APIException
+from app import app, db, gestor_de_inicio_de_sesion
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, login_required
 
-from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required, get_jwt
-from app.models import db, Comprador, Vendedor,Moderador,Producto
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_mail import Mail, Message
-from datetime import datetime, timedelta
-import uuid
-from flask import jsonify
-from datetime import datetime
-
-app = Blueprint('app', __name__)
-
-mail = Mail()
-
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    nombre = data['nombre']
-    email = data['email']
-    password = data['password']
-    rol = data['rol']
-
-    if rol not in ['comprador', 'moderador', 'vendedor']:
-        return jsonify({'message': 'Rol no válido'}), 400
-
-    comprador = Comprador.query.filter_by(nombre=nombre).first()
-    if user:
-        return jsonify({'message': 'Username already exists'}), 400
-
-    comprador = Comprador(nombre, email, password, rol)
-    db.session.add(comprador)
-    db.session.commit()
-
-    if role == 'moderador':
-        moderador = Moderador(user_id=user.id)
-        db.session.add(moderador)
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "POST":
+        email = request.form["correo_electronico"]
+        password = request.form["contraseña"]
+        rol_id = request.form["rol"]
+        rol = Rol.query.filter_by(nombre=nombre_rol).first()
+        if not rol:
+            rol = Rol(nombre=rol_id)
+            db.session.add(rol)
+            db.session.commit()
+        usuario = Usuario(email=email, rol_id=rol.id)
+        usuario.establecer_pass(password)
+        db.session.add(usuario)
         db.session.commit()
-    elif rol == 'vendedor':
-        Vendedor = Vendedor(user_id=user.id)
-        db.session.add(comprador)
-        db.session.commit()
+        return redirect(url_for("inicio_sesion"))
+    return render_template("registro.html")
 
-    return jsonify({'message': 'Usuario creado de forma exitosa'}), 201
+@app.route("/inicio_sesion", methods=["GET", "POST"])
+def inicio_sesion():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        usuario = Usuario.query.filter_by(email=correo_electronico).first()
+        if usuario and usuario.verificar_pass(password):
+            login_user(usuario)
+            return redirect(url_for("index"))
+    return render_template("inicio_sesion.html")
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    nombre = data['nombre']
-    password = data['password']
+@app.route("/cerrar_sesion")
+@login_required
+def cerrar_sesion():
+    logout_user()
+    return redirect(url_for("inicio_sesion"))
 
-    comprador = Comprador.query.filter_by(nombre=nombre).first()
-    if not comprador:
-        return jsonify({'message': 'La contraseña es incorrecta'}), 401
+@app.route("/")
+@login_required
+def index():
+    return "Bienvenido, {}!".format(current_user.email)
 
-    if not comprador.check_password(password):
-        return jsonify({'message': 'El nombre de usuario o la constraseña es incorrecta'}), 401
-
-    access_token = create_access_token(identity=nombre)
-    return jsonify({'access_token': access_token}), 200
-
-@app.route('/protected', methods=['GET'])
-@jwt_required
-def protected():
-    username = get_jwt_identity()
-    return jsonify({'message': f'Hola, {nombre}!'}), 200
+if __name__ == "__main__":
+    app.run(debug=True)
